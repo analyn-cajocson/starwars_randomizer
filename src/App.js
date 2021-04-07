9eeb28ef0ff2909e6c3ae61ce9e9855f11327986
Analyn Cajocson <analyncajocson@ymail.com>
2021-04-07 14:10:56 +0800

    Adds starwars characters randomizer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class FilmItemRow extends React.Component {
+  render(){
+    return(
+      <li>
+        <a href={this.props.url}>{this.props.url}</a>
+      </li>
+    )
+  }
+}
+
+class StarWars extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      name: null,
+      height: null,
+      homeworld: null,
+      films: [],
+      loadedCharacter: false
+    }
+  }
+
+  getNewCharacter() {
+    const randomNumber = Math.round( Math.random() * 82)
+    const url = `https://swapi.dev/api/people/${randomNumber}/`
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          name: data.name,
+          height: data.height,
+          homeworld: data.homeworld,
+          films: data.films,
+          loadedCharacter: true
+        })
+      })
+  }
+
+
+  render(){
+    
+    const movies = this.state.films.map((url, i) => {
+      return <FilmItemRow key={i} url={url} />
+    })
+
+    return(
+      <div>
+      {
+        this.state.loadedCharacter &&
+        <div>
+          <h1>{this.state.name}</h1>
+          <p>{this.state.height} cm</p>
+          <p> <a href={this.state.homeworld}>Homeworld</a></p>
+          <div>
+              <ul>
+                 {
+                   this.state.films.length > 0 &&
+                   <div>
+                     {movies}
+                   </div>
+                 }
+              </ul>
+          </div>
+        </div>
+      }
+        <button type="button" onClick={() => this.getNewCharacter()} className="btn">Randomize Character</button>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StarWars />
       </header>
     </div>
   );
