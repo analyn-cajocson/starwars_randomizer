beca8420a87175df27cdd299d7d21e67044ffcfe
Analyn Cajocson <analyncajocson@ymail.com>
2021-04-08 11:37:53 +0800

    Adds new button for vote Adds className for the new button Adds functionality that shows and add votes for every character Show character ID for the mean time

--- src/App.js
+++ src/App.js
@@ -16,12 +16,14 @@ class StarWars extends React.Component {
   constructor(){
     super()
     this.state = {
+      id: null,
       name: null,
       height: null,
       homeworld: null,
       image: null,
       affiliations: [],
-      loadedCharacter: false
+      loadedCharacter: false,
+      vote: 0
     }
   }
 
@@ -34,16 +36,24 @@ class StarWars extends React.Component {
       .then(response => response.json())
       .then(data => {
         this.setState({
+          id: data.id,
           name: data.name,
           height: data.height,
           homeworld: data.homeworld,
           image: data.image,
           affiliations: data.affiliations,
-          loadedCharacter: true
+          loadedCharacter: true,
+          vote: (data.id in localStorage) ? parseInt(localStorage.getItem(data.id)) : 0
         })
       })
   }
 
+  upvoteCharacter() {
+    this.setState({
+      vote: this.state.vote + 1
+    })
+    localStorage.setItem(this.state.id, this.state.vote + 1);
+  }
 
   render(){
     
@@ -58,6 +68,8 @@ class StarWars extends React.Component {
         <div>
           <img className="character-img" src={this.state.image} alt={this.state.name} />
           <h1>Name: {this.state.name}</h1>
+          <h3>ID: {this.state.id}</h3>
+          <p>Vote: {this.state.vote}</p>
           <p>Height: {this.state.height} cm</p>
           <p>Homeworld: {this.state.homeworld}</p>
           <div>
@@ -70,6 +82,9 @@ class StarWars extends React.Component {
                    </div>
                  }
               </ul>
+          </div>
+          <div>
+            <button type="button" onClick={() => this.upvoteCharacter()} className="btn btn-vote">Vote</button>
           </div>
         </div>
       }
