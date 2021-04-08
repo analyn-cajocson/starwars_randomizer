61d78cf8dba8b186708a6511f851e89bf06295e2
Analyn Cajocson <analyncajocson@ymail.com>
2021-04-08 14:57:13 +0800

    Fix header

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>StarWars Character Face-off</h1>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">About</a></li>
+        </ul>
       </header>
 
       <section className="main-content">
